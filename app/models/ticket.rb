0a43e3aaab87626c02abfb4e1a0004e29263426b
MartinUrrutia <mfurrutia@miuandes.cl>
2020-04-10 22:35:54 -0400

    Callbacks

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,12 +2,20 @@ class Ticket < ApplicationRecord
   belongs_to :order
   belongs_to :ticket_type
 
-  # TODO: complete the folowing
-  # before_xxxxx :update_stats
+  before_save :update_stats
+  before_destroy :update_stats
 
   private
     def update_stats
       es = self.ticket_type.event.event_stat
-      # TODO: complete in order to update event stats
+      if ticket.save?
+        self.event_stat=( es.tickets_sold+1), (es.attendance +1)
+      end
+      if ticket.destroy?
+        self.event_stat=( es.tickets_sold-1), (es.attendance -1)
+      end
+      if es.tickets_sold>es.capacity?
+        errors.add(:capacity, "the number of tickets sold exceeds the capacity of the event venue")
+      end
     end
 end
